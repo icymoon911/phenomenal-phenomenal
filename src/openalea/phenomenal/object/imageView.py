0ecaabafd782f3e8e974e1e49b48065ce8a18461
Christian Fournier <Christian.Fournier@inra.fr>
2026-05-27 15:41:44 +0200

    add support functions for image_paths dict to be used as inputs for imageviews constructs

--- src/openalea/phenomenal/object/imageView.py
+++ src/openalea/phenomenal/object/imageView.py
@@ -19,14 +19,67 @@ class ImageView:
         self.integral = None
 
 
-def get_image_views(image_paths, calibration, imread):
-    im_views = dict()
+def iter_image_paths(image_paths, imread, cameras=None, angles=None):
+
+    if cameras is not None:
+        cameras = set(cameras)
+
+    if angles is not None:
+        angles = set(angles)
+
     for id_camera in image_paths:
+
+        if cameras is not None and id_camera not in cameras:
+            continue
+
         for angle in image_paths[id_camera]:
-            name = f'{id_camera}_{angle}'
-            projection = calibration.get_projection(id_camera, angle)
-            im_views[name] = ImageView(
-                imread(image_paths[id_camera][angle]),
-                projection
-            )
+
+            if angles is not None and angle not in angles:
+                continue
+
+            yield id_camera, angle, imread(image_paths[id_camera][angle])
+
+
+def iter_images(images, cameras=None, angles=None):
+
+    if cameras is not None:
+        cameras = set(cameras)
+
+    if angles is not None:
+        angles = set(angles)
+
+    for id_camera in images:
+
+        if cameras is not None and id_camera not in cameras:
+            continue
+
+        for angle in images[id_camera]:
+
+            if angles is not None and angle not in angles:
+                continue
+
+            yield id_camera, angle, images[id_camera][angle]
+
+
+def as_image_views(images_iterator, calibration):
+    """Create an ImageView dict from images dict and calibration object
+
+    Args:
+        - images_iterator : a (camera_id, angle, image_array) iterator. See iter_images
+            or iter_image_paths in openalea.phenomenal.object.
+        - calibration: a phenomenal.calibration.Calibration object
+
+    Returns:
+        a {f'{camera_id}_{view_angle}': openalea.phenomenal.object.ImageView, ...} dict
+
+    """
+    im_views = dict()
+    for id_camera, angle, image in images_iterator:
+        name = f'{id_camera}_{angle}'
+        projection = calibration.get_projection(id_camera, angle)
+        im_views[name] = ImageView(
+            image,
+            projection
+        )
     return im_views
+
